ae51532eccd1166faa4b92f6545ef4ef9b88e931
adhirajpandey <pandey.adhiraj02@gmail.com>
2024-02-18 00:22:22 +0530

    Add Basic Functionality

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-const express = require("express")
-const cors = require("cors")
+import express from "express"
+import cors from "cors"
+import { generateClipId } from "./utils.js"
+import connectDB from "./db.js"
+import mongoose from "mongoose"
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const clipboardSchema = new mongoose.Schema({
+    clipId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    clipString: {
+        type: String,
+        required: true,
+    },
+},
+{
+    versionKey: false
+});
+
+const Clipboard = mongoose.model('clips', clipboardSchema);
+
+
 const app = express()
+
 
 app.use(express.json())
 app.use(cors())
@@ -10,14 +43,30 @@ app.get("/", (req, resp) => {
     resp.sendFile(__dirname + "/index.html")
 })
 
-app.post("/save", (req, resp) => {
+app.post("/save", async (req, resp) => {
     const clipboardText = req.body.clipboardText
+    const clipId = generateClipId(8)
+
+    const newClipboardItem = new Clipboard({
+        clipId: clipId,
+        clipString: clipboardText,
+    })
+
+    const response = await newClipboardItem.save()
+    
     resp.json({
-        "msg": "Saved"
+        "clip": response
     })
 })
 
 
-app.listen(8000, () => {
-    console.log("Now listening on port 8000....")
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("\n Server is running at port : 8000")
+    })
 })
+.catch((err) => {
+    console.log("MONGO DB initial connection failed !!! ", err)
+})
+
---
+++ utils.js
@@ -0,0 +1,11 @@
+export function generateClipId(length) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let result = ''
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * alphabet.length)
+        result += alphabet.charAt(randomIndex)
+    }
+
+    return result
+}
